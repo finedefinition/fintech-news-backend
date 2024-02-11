20abd70cab9223545eda74e6f7fd545da65cb35a
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2024-02-11 23:40:38 +0000

    Set CORS options

--- src/main/java/org/example/fintechnewsbackend/config/CorsConfig.java
+++ src/main/java/org/example/fintechnewsbackend/config/CorsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
     @Bean
@@ -13,10 +15,11 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
-        // !WARNING! Allow requests from all origins
-        config.addAllowedOrigin("*");
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://www.yourproductiondomain.com"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
+        config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+        config.setAllowCredentials(true); // Only if you need to send credentials
+        // config.setExposedHeaders(Arrays.asList("X-Custom-Header")); // If you need the client to read custom headers
 
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
